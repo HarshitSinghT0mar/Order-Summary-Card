194506544b2198e08ba03a686085190796c6c7e1
Harshit Singh Tomar <harshit.nitmz@gmail.com>
2023-08-09 23:07:20 +0530

    add pale blue bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
       backgroundImage: {
         "desktop-pattern": "url('./images/pattern-background-desktop.svg')",
         "mobile-pattern": "url('./images/pattern-background-mobile.svg')",
+
       },
+      backgroundColor:{
+       "pale-blue-color":" hsl(225, 100%, 98%)"
+      }
     },
   },
   plugins: [],
